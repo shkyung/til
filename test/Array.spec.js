6b59d2ff707eea2157508be126c1c3d461d9e7fc
sunny kyung <shkyung@lunit.io>
2020-05-29 00:39:46 +0900

    add Array.copyWithin(), entries() testcases

--- test/Array.spec.js
+++ test/Array.spec.js
@@ -15,7 +15,23 @@ describe('Array test', () => {
     expect(Array.isArray(arr)).toBeTruthy();
   })
   test('Array.concat()', () => {
-    expect([1,2].concat([[3,4],[5,6]])).not.toContain(3);
-    expect([1,2].concat([[3,4],[5,6]])).toContain(1);
+    expect([1, 2].concat([[3, 4], [5, 6]])).not.toContain(3);
+    expect([1, 2].concat([[3, 4], [5, 6]])).toContain(1);
+  })
+
+  test('Array.copyWithin()', () => {
+    expect(['a', 'b', 'c', 'd', 'e'].copyWithin(1, 2, 4)).toEqual(['a', 'c', 'd', 'd', 'e']);
+    expect([1, 2, 3, 4, 5].copyWithin(-1)).toEqual([1, 2, 3, 4, 1]);
+    expect([1, 2, 3, 4, 5].copyWithin(-4, -3, -1)).toEqual([1, 3, 4, 4, 5]);
+    //start, end index 가 역순일 경우 동작하지 않는다.
+    expect([1, 2, 3, 4, 5].copyWithin(-4, -1, -3)).toEqual([1, 2, 3, 4, 5]);
+  })
+
+  test('Array.entries()', () => {
+    const fruits = ["Banana", "Orange"];
+    const f = fruits.entries();
+    expect(f.next().value).toEqual([0, "Banana"])
+    expect(f.next().value).toEqual([1, "Orange"])
+    expect(f.next().done).toBeTruthy()
   })
 })
