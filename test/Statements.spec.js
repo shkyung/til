58ff894ece7f088dec884c144bd58885e0aac63e
sunny kyung <shkyung@lunit.io>
2020-07-07 02:08:38 +0900

    continue, function*

---
+++ test/Statements.spec.js
@@ -0,0 +1,26 @@
+describe('Statements test', () => {
+  test('continue ', () => {
+    let text = '';
+
+    for (let i = 0; i < 10; i++) {
+      if (i === 3) {
+        continue;
+      }
+      text += i;
+    }
+    expect(text).toBe('012456789')
+  })
+
+  test('function* #1', () => {
+    function* generator(i) {
+      yield i;
+      yield i + 10;
+    }
+
+    const gen = generator(10)
+
+    expect(gen.next().value).toBe(10)
+    expect(gen.next().value).toBe(20)
+    expect(gen.next().value).toBeUndefined()
+  })
+})
