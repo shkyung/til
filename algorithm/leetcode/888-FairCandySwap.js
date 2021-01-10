bc2cf031fd34df6c1e518379f0a71fae63ebd3e2
sunny kyung <shkyung@lunit.io>
2021-01-11 01:24:25 +0900

    888.fair candy swap - apply feedback

--- algorithm/leetcode/888-FairCandySwap.js
+++ algorithm/leetcode/888-FairCandySwap.js
@@ -1,6 +1,7 @@
 /**
  * https://leetcode.com/problems/fair-candy-swap
- * Time Limit exceeded (not passed yet)
+ * Runtime: 484 ms, faster than 28.90% of JavaScript online submissions for Fair Candy Swap.
+ * Memory Usage: 43.1 MB, less than 95.95% of JavaScript online submissions for Fair Candy Swap.
  */
 /**
  * @param {number[]} A
@@ -11,17 +12,18 @@ var fairCandySwap = function (A, B) {
   let isFound = false
   let exchangeA = 0
   let exchangeB = 0
+  const sumOfA = getSumInArr(A)
+  const sumOfB = getSumInArr(B)
+  const lengthA = A.length
+  const lengthB = B.length
 
-  for (let i = 0; i < A.length; i++) {
+  for (let i = 0; i < lengthA; i++) {
     const targetA = A[i]
-    for (let j = 0; j < B.length; i++) {
-      const targetB = B[j]
-      const newA = [...A]
-      const newB = [...B]
-      newA[i] = targetB
-      newB[j] = targetA
 
-      if (getSumInArr(newA) === getSumInArr(newB)) {
+    for (let j = 0; j < lengthB; j++) {
+      const targetB = B[j]
+
+      if (sumOfA - targetA + targetB === sumOfB - targetB + targetA) {
         isFound = true
         exchangeA = targetA
         exchangeB = targetB
