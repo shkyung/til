c7e35a652aecbfff1f84d5532b2394a981a09b28
sunny kyung <shkyung@lunit.io>
2021-01-15 16:37:53 +0900

    1395-count-number-of-teams 피드백 적용

--- algorithm/leetcode/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ algorithm/leetcode/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -1,19 +1,17 @@
 /***
- * https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/submissions/
- * Runtime: 92 ms, faster than 89.80% of JavaScript online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
- * Memory Usage: 45.7 MB, less than 51.02% of JavaScript online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+ * Runtime: 96 ms, faster than 83.74% of JavaScript online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+ * Memory Usage: 45.9 MB, less than 50.49% of JavaScript online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
  */
 /**
  * @param {string} n
  * @return {number}
  */
 var minPartitions = function (n) {
-  const arr = ('' + n).split('')
-  let i = 0
+  const arr = n.split('')
   let length = arr.length
   let max = 0
 
-  for (i; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     const int = parseInt(arr[i])
     if (int > max) {
       max = int
