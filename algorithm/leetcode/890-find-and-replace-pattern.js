912dde843bb10044853e7da9c3295d114aa1e0f1
sunny kyung <shkyung@lunit.io>
2021-01-25 03:28:26 +0900

    refactor solution

--- algorithm/leetcode/890-find-and-replace-pattern.js
+++ algorithm/leetcode/890-find-and-replace-pattern.js
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/find-and-replace-pattern
- * Runtime: 88 ms, faster than 41.30% of JavaScript online submissions for Find and Replace Pattern.
- * Memory Usage: 41.3 MB, less than 26.09% of JavaScript online submissions for Find and Replace Pattern.
+ * Runtime: 76 ms, faster than 95.41% of JavaScript online submissions for Find and Replace Pattern.
+ * Memory Usage: 41.2 MB, less than 30.28% of JavaScript online submissions for Find and Replace Pattern.
  */
 /**
  * @param {string[]} word
@@ -9,24 +9,29 @@
  * @return {string[]}
  */
 var findAndReplacePattern = function (words, pattern) {
-  const ret = words.reduce((accum, word) => {
-    let map = {}
+  function _getTransformedString(str) {
+    const map = {}
+    let count = 0
 
-    const changedWord = [...word]
-      .map((spell, i) => {
-        if (!map[pattern[i]] && !Object.values(map).includes(spell)) {
-          map[pattern[i]] = spell
+    const transformedStr = [...str]
+      .map((spell) => {
+        if (!map[spell]) {
+          count += 1
+          map[spell] = String(count)
         }
-        const match = Object.keys(map).find((key) => map[key] === spell)
 
-        if (match) {
-          spell = spell.replace(new RegExp(spell), match)
-        }
-        return spell
+        return map[spell]
       })
       .join('')
+    return transformedStr
+  }
 
-    if (changedWord === pattern) {
+  const transformedPattern = _getTransformedString(pattern)
+
+  const ret = words.reduce((accum, word) => {
+    const transformedWord = _getTransformedString(word)
+
+    if (transformedWord === transformedPattern) {
       accum.push(word)
     }
     return accum
