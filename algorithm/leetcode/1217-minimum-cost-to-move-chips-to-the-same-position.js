a282e6cfe0210e3ff70fb3f01b66114ef9ba1073
sunny kyung <shkyung@lunit.io>
2021-01-25 03:52:53 +0900

    refactor solution

--- algorithm/leetcode/1217-minimum-cost-to-move-chips-to-the-same-position.js
+++ algorithm/leetcode/1217-minimum-cost-to-move-chips-to-the-same-position.js
@@ -1,54 +1,23 @@
 /**
  * https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position
- * Runtime: 136 ms, faster than 6.77% of JavaScript online submissions for Minimum Cost to Move Chips to The Same Position.
- * Memory Usage: 40.1 MB, less than 7.29% of JavaScript online submissions for Minimum Cost to Move Chips to The Same Position.
+ * Runtime: 76 ms, faster than 80.73% of JavaScript online submissions for Minimum Cost to Move Chips to The Same Position.
+ * Memory Usage: 38.4 MB, less than 76.04% of JavaScript online submissions for Minimum Cost to Move Chips to The Same Position.
  * */
 /**
  * @param {number[]} position
  * @return {number}
  */
 var minCostToMoveChips = function (position) {
-  const maxIndex = Math.max(...position)
-  const minIndex = Math.min(...position)
+  let movingNumOfEven = 0
+  let movingNumOfOdd = 0
 
-  const obj = position.reduce((accum, position) => {
-    if (!accum[position]) {
-      accum[position] = 0
+  position.forEach((position) => {
+    if (position % 2 === 0) {
+      movingNumOfEven += 1
+    } else {
+      movingNumOfOdd += 1
     }
-    accum[position] += 1
-
-    return accum
-  }, {})
-
-  let max = 0
-  const candidate = []
-  let maxPosition = 0
-
-  for (key in obj) {
-    if (obj[key] > max) {
-      max = obj[key]
-      maxPosition = key
-    }
-  }
-  candidate.push(maxPosition)
-
-  for (key in obj) {
-    if (key !== maxPosition && obj[key] === max) {
-      candidate.push(key)
-    }
-  }
-
-  candidate.push(Math.floor((maxIndex + minIndex) / 2))
-
-  const costArr = candidate.map((position) => {
-    let cost = 0
-    for (key in obj) {
-      if (key !== position) {
-        cost += (Math.abs(+position - +key) % 2) * obj[key]
-      }
-    }
-    return cost
   })
 
-  return Math.min(...costArr)
+  return Math.min(movingNumOfEven, movingNumOfOdd)
 }
