a547ac62b44f0b7111cdaba267bfaa5befc7c3e6
sunny kyung <shkyung@lunit.io>
2021-02-09 10:31:33 +0900

    1486-xor-operation-in-an-array.js - feedback

--- algorithm/leetcode/2021-02/1486-xor-operation-in-an-array.js
+++ algorithm/leetcode/2021-02/1486-xor-operation-in-an-array.js
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/xor-operation-in-an-array
- * Runtime: 72 ms, faster than 94.26% of JavaScript online submissions for XOR Operation in an Array.
- * Memory Usage: 38.6 MB, less than 45.41% of JavaScript online submissions for XOR Operation in an Array.
+ * Runtime: 76 ms, faster than 80.03% of JavaScript online submissions for XOR Operation in an Array.
+ * Memory Usage: 38.4 MB, less than 59.42% of JavaScript online submissions for XOR Operation in an Array.
  */
 /**
  * @param {number} n
@@ -9,14 +9,11 @@
  * @return {number}
  */
 var xorOperation = function (n, start) {
-  let ret = null
-  for (let i = 0; i < n; i++) {
-    const num = start + 2 * i
-    if (ret == null) {
-      ret = num
-    } else {
-      ret ^= num
-    }
+  let prev = start
+  let ret = prev
+  for (let i = 1; i < n; i++) {
+    prev += 2
+    ret ^= prev
   }
   return ret
 }
