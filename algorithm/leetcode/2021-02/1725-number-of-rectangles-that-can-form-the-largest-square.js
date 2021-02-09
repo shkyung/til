e3c02484ad7dd3047ff5f3e70f8bb420f3e22b37
sunny kyung <shkyung@lunit.io>
2021-02-09 11:02:28 +0900

    Optimization

--- algorithm/leetcode/2021-02/1725-number-of-rectangles-that-can-form-the-largest-square.js
+++ algorithm/leetcode/2021-02/1725-number-of-rectangles-that-can-form-the-largest-square.js
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square
- * Runtime: 88 ms, faster than 83.82% of JavaScript online submissions for Number Of Rectangles That Can Form The Largest Square.
- * Memory Usage: 42.6 MB, less than 22.69% of JavaScript online submissions for Number Of Rectangles That Can Form The Largest Square.
+ * Runtime: 88 ms, faster than 83.43% of JavaScript online submissions for Number Of Rectangles That Can Form The Largest Square.
+ * Memory Usage: 41.6 MB, less than 57.17% of JavaScript online submissions for Number Of Rectangles That Can Form The Largest Square.
  */
 /**
  * @param {number[][]} rectangles
@@ -9,18 +9,16 @@
  */
 var countGoodRectangles = function (rectangles) {
   let max = 0
-  const squares = rectangles.reduce((accum, rectangle) => {
+  const number = rectangles.reduce((accum, rectangle) => {
     const [l, w] = rectangle
     const squareLen = Math.min(l, w)
-    max = Math.max(max, squareLen)
-    accum.push(squareLen)
-    return accum
-  }, [])
-
-  return squares.reduce((accum, squareLen) => {
-    if (squareLen === max) {
+    if (squareLen > max) {
+      max = squareLen
+      accum = 1
+    } else if (squareLen === max) {
       accum += 1
     }
     return accum
   }, 0)
+  return number
 }
