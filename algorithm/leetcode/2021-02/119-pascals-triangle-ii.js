e1afa82eec6b311faa5c253a61b013397512f1ab
sunny kyung <shkyung@lunit.io>
2021-02-12 01:45:56 +0900

    apply recursion and Map()

--- algorithm/leetcode/2021-02/119-pascals-triangle-ii.js
+++ algorithm/leetcode/2021-02/119-pascals-triangle-ii.js
@@ -7,19 +7,59 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-var getRow = function (rowIndex) {
-  const pascalArr = [[1], [1, 1]]
+const rowMap = new Map()
+rowMap.set(0, [1])
+rowMap.set(1, [1, 1])
 
-  if (rowIndex > 1) {
-    for (let i = 2; i <= rowIndex; i++) {
-      const prevArr = pascalArr[i - 1]
-      const prevArrLen = prevArr.length
-      pascalArr[i] = [1]
-      for (let j = 0; j < prevArrLen - 1; j++) {
-        pascalArr[i].push(prevArr[j] + prevArr[j + 1])
-      }
-      pascalArr[i].push(1)
-    }
+// version 1
+var getRow = function (rowIndex) {
+  if (rowMap.has(rowIndex)) {
+    return rowMap.get(rowIndex)
   }
-  return pascalArr[rowIndex]
+  const prevArr = getRow(rowIndex - 1)
+  const curArr = new Array(rowIndex + 1)
+  curArr[0] = 1
+  curArr[rowIndex] = 1
+  const condition = prevArr.length - 1
+  for (let j = 0; j < condition; j++) {
+    curArr[j + 1] = prevArr[j] + prevArr[j + 1]
+  }
+  rowMap.set(rowIndex, curArr)
+
+  return curArr
+}
+
+// version2
+var getRow2 = function (rowIndex) {
+  if (rowMap.has(rowIndex)) {
+    return rowMap.get(rowIndex)
+  }
+  const prevArr = getRow(rowIndex - 1)
+  const curArr = Array.from({ length: rowIndex + 1 })
+  curArr[0] = 1
+  curArr[rowIndex] = 1
+  const condition = prevArr.length - 1
+  for (let j = 0; j < condition; j++) {
+    curArr[j + 1] = prevArr[j] + prevArr[j + 1]
+  }
+  rowMap.set(rowIndex, curArr)
+
+  return curArr
+}
+
+// version3
+var getRow = function (rowIndex) {
+  if (rowMap.has(rowIndex)) {
+    return rowMap.get(rowIndex)
+  }
+  const prevArr = getRow(rowIndex - 1)
+  const curArr = [1]
+  const condition = prevArr.length - 1
+  for (let j = 0; j < condition; j++) {
+    curArr.push(prevArr[j] + prevArr[j + 1])
+  }
+  curArr.push(1)
+  rowMap.set(rowIndex, curArr)
+
+  return curArr
 }
